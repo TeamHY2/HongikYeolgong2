414805a16029de86929d6d5c82539fd5913bad03
Choe-ju <juwon3415@gmail.com>
2025-02-19 13:47:19 +0900

    fix: 캘린더 셀 배경 이미지 방식 수정(임시) 및 기록 없는 날짜 비활성화 해제

--- HongikYeolgong2/Presentation/Record/Component/CalendarCell.swift
+++ HongikYeolgong2/Presentation/Record/Component/CalendarCell.swift
@@ -52,15 +52,60 @@ struct CalendarCell: View {
                     .foregroundStyle(getForegroundStyle())
             }
             .frame(width: 38.adjustToScreenWidth,height: 38.adjustToScreenHeight)
-            .background(Image(getImageForCellStyle())
-                .resizable()
-                .frame(width: 48.adjustToScreenWidth,height: 48.adjustToScreenHeight))
-            .cornerRadius(8)
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                            .fill(AngularGradient(gradient: Gradient(colors: getImageForCellStyle()), center: .center,startAngle: .degrees(-45), endAngle: .degrees(315)))
+                            .frame(width: 38.adjustToScreenWidth,height: 38.adjustToScreenHeight)
+//                Image(getImageForCellStyle())
+//                    .resizable()
+//                    .frame(width: 48.adjustToScreenWidth,height: 48.adjustToScreenHeight)
+            )
             .opacity(isVisible ? 0 : 1)
-            .overlay(isVisible || isSelected ? nil : Color.dark.opacity(0.6))
+//            .overlay(isVisible || isSelected ? nil : Color.dark.opacity(0.8))
             .animation(.easeInOut(duration: 0.2), value: isSelected)
         }
-        .disabled(isVisible || cellStyle == .dayCount00)
+        .disabled(isVisible)
+    }
+    
+    // 배경 색상 반환
+    private func getImageForCellStyle() -> [Color] {
+        switch cellStyle {
+            case .dayCount00:
+                return [.gray800.opacity(isSelected ? 1 : 0.3)]
+            case .dayCount01:
+                return [Color(.sRGB, red: 19/255, green: 30/255, blue: 72/255, opacity: isSelected ? 1 : 0.2),
+                        Color(.sRGB, red: 25/255, green: 38/255, blue: 88/255, opacity: isSelected ? 1 : 0.2)]
+            case .dayCount02:
+                return [Color(.sRGB, red: 51/255, green: 79/255, blue: 178/255, opacity: isSelected ? 1 : 0.1),
+                        Color(.sRGB, red: 83/255, green: 115/255, blue: 227/255, opacity: isSelected ? 1 : 0.1)]
+            case .dayCount03:
+                return [Color(.sRGB, red: 204/255, green: 246/255, blue: 48/255, opacity: isSelected ? 1 : 0.05),
+                        Color(.sRGB, red: 244/255, green: 253/255, blue: 61/255, opacity: isSelected ? 1 : 0.05)]
+//            case .dayCount00:
+//                return [.gray800.opacity(1)]
+//            case .dayCount01:
+//                return [Color(.sRGB, red: 19/255, green: 30/255, blue: 72/255, opacity: 1),
+//                        Color(.sRGB, red: 25/255, green: 38/255, blue: 88/255, opacity: 1)]
+//            case .dayCount02:
+//                return [Color(.sRGB, red: 51/255, green: 79/255, blue: 178/255, opacity: 1),
+//                        Color(.sRGB, red: 83/255, green: 115/255, blue: 227/255, opacity: 1)]
+//            case .dayCount03:
+//                return [Color(.sRGB, red: 204/255, green: 246/255, blue: 48/255, opacity: 1),
+//                        Color(.sRGB, red: 244/255, green: 253/255, blue: 61/255, opacity: 1)]
+        }
+    }
+    
+    private func getForBorderStyle() -> Color {
+        switch cellStyle {
+            case .dayCount00:
+                return .gray800
+            case .dayCount01:
+                return .blue400
+            case .dayCount02:
+                return .blue200
+            case .dayCount03:
+                return Color(.sRGB, red: 206/255, green: 207/255, blue: 55/255, opacity: 1)
+        }
     }
     
     private func getImageForCellStyle() -> ImageResource {
@@ -74,10 +119,14 @@ struct CalendarCell: View {
     
     private func getForegroundStyle() -> Color {
         switch cellStyle {
-            case .dayCount00: return .gray300
-            case .dayCount01: return .gray100
-            case .dayCount02: return .white
-            case .dayCount03: return .gray600
+            case .dayCount00: return .gray300.opacity(isSelected ? 1 : 0.3)
+            case .dayCount01: return .gray100.opacity(isSelected ? 1 : 0.3)
+            case .dayCount02: return .white.opacity(isSelected ? 1 : 0.3)
+            case .dayCount03: return .gray600.opacity(isSelected ? 1 : 0.3)
+//            case .dayCount00: return .gray300
+//            case .dayCount01: return .gray100
+//            case .dayCount02: return .white
+//            case .dayCount03: return .gray600
         }
     }
 }
