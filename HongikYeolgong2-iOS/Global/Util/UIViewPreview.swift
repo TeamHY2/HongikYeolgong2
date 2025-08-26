12c74e188126c0a5fc805f092c529442edc45ca7
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-08-26 18:14:36 +0900

    add: Preview 코드 추가

---
+++ HongikYeolgong2-iOS/Global/Util/UIViewPreview.swift
@@ -0,0 +1,28 @@
+//
+//  UIViewPreview.swift
+//  HongikYeolgong2-iOS
+//
+//  Created by 권석기 on 8/26/25.
+//
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+struct UIViewPreview<View: UIView>: UIViewRepresentable {
+    let view: View
+    
+    init(_ builder: @escaping () -> View) {
+        view = builder()
+    }
+    
+    // MARK: - UIViewRepresentable
+    
+    func makeUIView(context: Context) -> UIView {
+        return view
+    }
+    
+    func updateUIView(_ view: UIView, context: Context) {
+        view.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        view.setContentHuggingPriority(.defaultHigh, for: .vertical)
+    }
+}
+#endif
