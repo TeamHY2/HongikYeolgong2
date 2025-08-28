026f755fa3afece154402fc85a4cc2248e1669a0
Choe-ju <juwon3415@gmail.com>
2025-08-29 04:49:03 +0900

    feat: GradientButton Style 추가 (좌석 버튼용) #5

--- HongikYeolgong2-iOS/Presentation/Component/GradientButton.swift
+++ HongikYeolgong2-iOS/Presentation/Component/GradientButton.swift
@@ -9,21 +9,61 @@ import UIKit
 import Then
 import SnapKit
 
+enum GradientButtonType {
+    case main // 기본 색상
+    case sub  // 서브 생상 (좌석 버튼)
+    
+    func bagroundColor(isEnabled: Bool = true)-> [CGColor] {
+        switch self {
+            case .main:
+                return isEnabled ?
+                    [   // 활성화 색상
+                        UIColor(red: 51/255, green: 79/255, blue: 178/255, alpha: 1).cgColor,
+                        UIColor(red: 83/255, green: 115/255, blue: 227/255, alpha: 1).cgColor
+                    ] : [   // 비활성화 색상
+                        UIColor(red: 19/255, green: 30/255, blue: 72/255, alpha: 1).cgColor,
+                        UIColor(red: 25/255, green: 38/255, blue: 88/255, alpha: 1).cgColor
+                    ]
+            case .sub:
+                return
+                    [
+                        UIColor(red: 24/255, green: 43/255, blue: 112/255, alpha: 1).cgColor,
+                        UIColor(red: 36/255, green: 60/255, blue: 143/255, alpha: 1).cgColor
+                    ]
+        }
+    }
+    
+    func borderColor(isEnabled: Bool = true)-> CGColor {
+        switch self {
+            case .main:
+                return isEnabled ? UIColor.blue200.cgColor: UIColor.blue400.cgColor
+            case .sub:
+                return UIColor.blue400.cgColor
+        }
+    }
+    
+    func fontColor(isEnabled: Bool) -> UIColor {
+        switch self {
+            case .main:
+                return isEnabled ? .white : .gray300
+            case .sub:
+                return .gray100
+        }
+    }
+    
+}
+
 /// # 그라데이션 색상 버튼
 /// - title : 버튼 제목
 /// - onTap : 버튼 클릭 액션
 class GradientButton: UIButton {
-    // 버튼 배경 색상
-    private let bagroundColor: [[CGColor]] = [
-        [   // 활성화 색상
-            UIColor(red: 51/255, green: 79/255, blue: 178/255, alpha: 1).cgColor,
-            UIColor(red: 83/255, green: 115/255, blue: 227/255, alpha: 1).cgColor
-        ],
-        [   // 비활성화 색상
-            UIColor(red: 19/255, green: 30/255, blue: 72/255, alpha: 1).cgColor,
-            UIColor(red: 25/255, green: 38/255, blue: 88/255, alpha: 1).cgColor
-        ]
-    ]
+    var buttonStyle: GradientButtonType = .main {
+        didSet {
+            gradientLayer.colors = buttonStyle.bagroundColor(isEnabled: isEnabled)
+            setTitleColor(buttonStyle.fontColor(isEnabled: isEnabled), for: .normal)
+            layer.borderColor = buttonStyle.borderColor(isEnabled: isEnabled)
+        }
+    }
     
     /// 버튼 제목
     var title: String? {
@@ -32,9 +72,8 @@ class GradientButton: UIButton {
         }
     }
     
-    let gradientLayer = CAGradientLayer().then {
-        $0.colors = [UIColor(red: 51/255, green: 79/255, blue: 178/255, alpha: 1).cgColor,
-                     UIColor(red: 83/255, green: 115/255, blue: 227/255, alpha: 1).cgColor]
+    private lazy var gradientLayer = CAGradientLayer().then {
+        $0.colors = buttonStyle.bagroundColor()
         $0.type = .conic
         $0.locations = [0.0, 0.7, 1.0]
         $0.startPoint = CGPoint(x: 0.5, y: 0.5)
@@ -47,13 +86,19 @@ class GradientButton: UIButton {
     override var isEnabled: Bool{
         didSet {
             // 활성화 여부에따른 색상 변경
-            gradientLayer.colors = isEnabled ? bagroundColor[0] : bagroundColor[1]
-            setTitleColor(isEnabled ? .white : .gray300, for: .normal)
-            layer.borderColor = isEnabled ? UIColor.blue200.cgColor: UIColor.blue400.cgColor
+            gradientLayer.colors = buttonStyle.bagroundColor(isEnabled: isEnabled)//isEnabled ? bagroundColor[0] : bagroundColor[1]
+            setTitleColor(buttonStyle.fontColor(isEnabled: isEnabled), for: .normal)
+            layer.borderColor = buttonStyle.borderColor(isEnabled: isEnabled)
         }
     }
     
     // MARK: - Initialization
+    // 스타일 지정 생성자
+    init(style: GradientButtonType = .main) {
+        self.buttonStyle = style
+        super.init(frame: .zero)
+        setupView()
+    }
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
