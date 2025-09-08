7de3567a247209a2a38232e64f8a51c66268283b
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-09-08 18:55:21 +0900

    feat: 뷰모델 상태 관리 로직 추가

--- HongikYeolgong2-iOS/Presentation/Home/VC/HomeViewController.swift
+++ HongikYeolgong2-iOS/Presentation/Home/VC/HomeViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import SnapKit
 import Then
+import Combine
 
 final class HomeViewController: UIViewController {
     
@@ -24,6 +25,10 @@ final class HomeViewController: UIViewController {
     // MARK: - Properties
     private let viewModel: HomeViewModel
     
+    private let loginButtonTapSubject = PassthroughSubject<Void, Never>.init()
+    private var cancellable = Set<AnyCancellable>()
+    private let userNameText = UILabel()
+    
     private lazy var loginButton = UIButton().then {
         $0.setTitle("Login", for: .normal)
         $0.setTitleColor(.label, for: .normal)
@@ -35,20 +40,41 @@ final class HomeViewController: UIViewController {
         super.viewDidLoad()
         addSubViews()
         setConstraint()
+        dataBind()
     }
-
+    
+    private func dataBind() {
+        let output = viewModel.transform(
+            HomeViewModel.Input(loginButtonTapped: loginButtonTapSubject.eraseToAnyPublisher())
+        )
+        
+        output.userName
+            .receive(on: DispatchQueue.main)
+            .assign(to: \.text, on: userNameText)
+            .store(in: &cancellable)
+    }
+    
     private func addSubViews() {
         view.addSubview(loginButton)
+        view.addSubview(userNameText)
     }
     
     private func setConstraint() {
         loginButton.snp.makeConstraints {
             $0.center.equalToSuperview()
         }
+        userNameText.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview()
+            $0.bottom.equalTo(loginButton.snp.bottom).offset(10)
+        }
+    }
+    
+    private func updateUserName(_ userName: String) {
+        self.userNameText.text = userName
     }
     
     @objc
     private func loginButtonTapped() {
-        print("tapped")
+        loginButtonTapSubject.send()
     }
 }
--- HongikYeolgong2-iOS/Presentation/Home/ViewModel/HomeViewModel.swift
+++ HongikYeolgong2-iOS/Presentation/Home/ViewModel/HomeViewModel.swift
@@ -5,11 +5,34 @@
 //  Created by 권석기 on 9/5/25.
 //
 
-final class HomeViewModel {
+import Combine
+
+final class HomeViewModel: ViewModelType {
+    
     private let userUseCase: UserUseCase
+    private var cancellable = Set<AnyCancellable>()
+    private let userNameSubject = CurrentValueSubject<String?, Never>("")
     
-    var userName: String = ""
+    // MARK: - Input & Output
+    struct Input {
+        let loginButtonTapped: AnyPublisher<Void, Never>
+    }
     
+    struct Output {
+        let userName: AnyPublisher<String?, Never>
+    }
+    
+    func transform(_ input: Input) -> Output {
+        input.loginButtonTapped.sink { [weak self] in
+            guard let self = self else { return }
+            userNameSubject.send("testUser")
+        }
+        .store(in: &cancellable)
+        
+       return Output(userName: userNameSubject.eraseToAnyPublisher())
+    }
+    
+    // MARK: - Initializer
     init(userUseCase: UserUseCase) {
         self.userUseCase = userUseCase
     }
